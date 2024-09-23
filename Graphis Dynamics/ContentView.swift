1f196735d03b074717b01063b30933136ba629e8
Erasmojf <erasmogohiper@gmail.com>
2024-09-23 15:45:34 -0300

    Initial Commit

---
+++ Graphis Dynamics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Graphis Dynamics
+//
+//  Created by Erasmo  on 23/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Graphis Dynamics/Graphis_DynamicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Graphis_DynamicsApp.swift
+//  Graphis Dynamics
+//
+//  Created by Erasmo  on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Graphis_DynamicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
